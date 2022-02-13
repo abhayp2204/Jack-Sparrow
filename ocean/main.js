5ddc978593abe5d9d2ea5266f2e1f694ae5af64f
AbhayPatil <abhay.patil@students.iiit.ac.in>
2022-02-13 16:51:24 +0530

    created-ocean

--- ocean/main.js
+++ ocean/main.js
@@ -1,4 +1,119 @@
-import 'style.css'
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import './style.css';
+import * as THREE from "three";
 
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { Water } from "three/examples/jsm/objects/Water.js";
+import { Sky } from "three/examples/jsm/objects/Sky.js";
+
+let camera, scene, renderer;
+let controls, water, sun;
+
+init();
+animate();
+
+function init() {
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  document.body.appendChild(renderer.domElement);
+
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    55,
+    window.innerWidth / window.innerHeight,
+    1,
+    20000
+  );
+  camera.position.set(30, 30, 100);
+
+  //
+
+  sun = new THREE.Vector3();
+
+  // Water
+
+  const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+
+  water = new Water(waterGeometry, {
+    textureWidth: 512,
+    textureHeight: 512,
+    waterNormals: new THREE.TextureLoader().load(
+      "textures/waternormals.jpg",
+      function (texture) {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+      }
+    ),
+    sunDirection: new THREE.Vector3(),
+    sunColor: 0xffffff,
+    waterColor: 0x001e0f,
+    distortionScale: 3.7,
+    fog: scene.fog !== undefined,
+  });
+
+  water.rotation.x = -Math.PI / 2;
+
+  scene.add(water);
+
+  // Skybox
+
+  const sky = new Sky();
+  sky.scale.setScalar(10000);
+  scene.add(sky);
+
+  const skyUniforms = sky.material.uniforms;
+
+  skyUniforms["turbidity"].value = 10;
+  skyUniforms["rayleigh"].value = 2;
+  skyUniforms["mieCoefficient"].value = 0.005;
+  skyUniforms["mieDirectionalG"].value = 0.8;
+
+  const parameters = {
+    elevation: 2,
+    azimuth: 180,
+  };
+
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+  function updateSun() {
+    const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
+    const theta = THREE.MathUtils.degToRad(parameters.azimuth);
+
+    sun.setFromSphericalCoords(1, phi, theta);
+
+    sky.material.uniforms["sunPosition"].value.copy(sun);
+    water.material.uniforms["sunDirection"].value.copy(sun).normalize();
+
+    scene.environment = pmremGenerator.fromScene(sky).texture;
+  }
+
+  updateSun();
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.maxPolarAngle = Math.PI * 0.495;
+  controls.target.set(0, 10, 0);
+  controls.minDistance = 40.0;
+  controls.maxDistance = 200.0;
+  controls.update();
+
+  const waterUniforms = water.material.uniforms;
+
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  render();
+}
+
+function render() {
+  water.material.uniforms["time"].value += 1.0 / 60.0;
+  renderer.render(scene, camera);
+}
--- vite-project/main.js
+++ vite-project/main.js
@@ -26,61 +26,47 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff0f7f});
-const torus = new THREE.Mesh(geometry, material);
+// const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+// const material = new THREE.MeshStandardMaterial({ color: 0xff0f7f});
+// const torus = new THREE.Mesh(geometry, material);
 
-scene.add(torus);
+// scene.add(torus);
 
-// Light
-const pointLight = new THREE.PointLight(0xffffff);
-const lightHelper = new THREE.PointLightHelper(pointLight);
-pointLight.position.set(10, 10, 10);
-scene.add(pointLight);
-scene.add(lightHelper);
+// // Light
+// const pointLight = new THREE.PointLight(0xffffff);
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// pointLight.position.set(10, 10, 10);
+// scene.add(pointLight);
+// scene.add(lightHelper);
 
-// Grid
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(gridHelper);
-const controls = new OrbitControls(camera, renderer.domElement);
+// // Grid
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(gridHelper);
+// const controls = new OrbitControls(camera, renderer.domElement);
 
-// Star
-function addStar() {
-    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-    const material = new THREE.MeshStandardMaterial({color: 0xffffff});
-    const star = new THREE.Mesh(geometry, material);
+// // Star
+// function addStar() {
+//     const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+//     const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+//     const star = new THREE.Mesh(geometry, material);
 
-    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-    star.position.set(x, y, z);
-    scene.add(star);
-};
-Array(200).fill().forEach(addStar);
+//     const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+//     star.position.set(x, y, z);
+//     scene.add(star);
+// };
+// Array(200).fill().forEach(addStar);
 
-// Space Texture
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
-scene.background = spaceTexture;
-    
-// Moon
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const moon = new THREE.Mesh(
-    new THREE.SphereGeometry(3, 32, 32),
-    new THREE.MeshBasicMaterial({map: moonTexture})
-);
-moon.position.z = 30;
-moon.position.setX(-10);
-scene.add(moon);
+// let speed = 0.01;
+// function animate() {
+//     requestAnimationFrame(animate);
 
-let speed = 0.01;
-function animate() {
-    requestAnimationFrame(animate);
+//     // Rotations
+//     // torus.rotation.x += 0.03;
+//     torus.rotation.y += speed;
+//     // torus.rotation.z += 0.01;
 
-    // Rotations
-    // torus.rotation.x += 0.03;
-    torus.rotation.y += speed;
-    // torus.rotation.z += 0.01;
+//     controls.update();
+//     renderer.render(scene, camera);
+// }
 
-    controls.update();
-    renderer.render(scene, camera);
-}
-
-animate()
+// animate()
