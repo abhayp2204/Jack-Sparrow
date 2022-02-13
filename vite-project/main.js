13af062bb633ddf408f7392ca062a9aec190dee1
AbhayPatil <abhay.patil@students.iiit.ac.in>
2022-02-13 13:26:58 +0530

    space-project

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,6 +1,7 @@
 // Imports
 import './style.css';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Create a Scene object named scene
 const scene = new THREE.Scene();
@@ -26,23 +27,59 @@ camera.position.setZ(30);
 renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0x349194});
+const material = new THREE.MeshStandardMaterial({ color: 0xff0f7f});
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
-const pointLight = new THREE.PointLight(0xffffff);;
+// Light
+const pointLight = new THREE.PointLight(0xffffff);
+const lightHelper = new THREE.PointLightHelper(pointLight);
 pointLight.position.set(10, 10, 10);
 scene.add(pointLight);
+scene.add(lightHelper);
 
+// Grid
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Star
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+    star.position.set(x, y, z);
+    scene.add(star);
+};
+Array(200).fill().forEach(addStar);
+
+// Space Texture
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+    
+// Moon
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(3, 32, 32),
+    new THREE.MeshBasicMaterial({map: moonTexture})
+);
+moon.position.z = 30;
+moon.position.setX(-10);
+scene.add(moon);
+
+let speed = 0.01;
 function animate() {
     requestAnimationFrame(animate);
 
     // Rotations
-    torus.rotation.x += 0.03;
-    torus.rotation.y += 0.01;
-    torus.rotation.z += 0.01;
+    // torus.rotation.x += 0.03;
+    torus.rotation.y += speed;
+    // torus.rotation.z += 0.01;
 
+    controls.update();
     renderer.render(scene, camera);
 }
 
