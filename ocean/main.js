2d822cacc7ca3111073fc26d7565c6c2a2eaf750
AbhayPatil <abhay.patil@students.iiit.ac.in>
2022-02-22 01:27:30 +0530

    add-cannon

--- ocean/main.js
+++ ocean/main.js
@@ -8,9 +8,7 @@ import { PerspectiveCamera } from "three";
 
 let camera;
 let scene;
-let radius = 10;
 let hoverFactor = 1;
-let hover = hoverFactor * radius;
 let renderer;
 let controls;
 let water;
@@ -20,10 +18,32 @@ let sign;
 let flag;
 
 let sunVol = 0.004;
-let horizonDistance = 100000;
+let horizonDistance = 5000;
 let vec = new THREE.Vector3(); 
-let boat;
-let boatSpeed = 20;
+
+// Ship
+let ship;
+let shipRadius = 10;
+let hover = hoverFactor * shipRadius;
+let shipSpeed = 3;
+
+// Pirate Ship
+let pirateShip;
+let pirateShipSpeed = 1;
+let pirateShipRadius = 10;
+let pirateShipAlive = true;
+const NUM_PIRATE_SHIPS = 5;
+const distanceFactor = 0.07;
+let targetDir;
+let minDistance;
+
+// Cannonball
+let cannonBall;
+let cannonBallRadius = 4;
+let cannonBallFired = false;
+const cannonBallSpeed = 30;
+let cannonDirection;
+
 const PI = 3.14159;
 
 // Keycodes
@@ -38,26 +58,197 @@ let east = new THREE.Vector3(0, 0, -1);
 const sky = new Sky();
 const skyUniforms = sky.material.uniforms;
 
+// Sample colors
+const darkGray = 0x222222;
+const black = 0x000000;
+const red = 0xAA0119;
+const yellow = 0xfadf1f;
+
+// Game colors
+const shipColor = yellow;
+const pirateShipColor = black;
+const cannonBallColor = red;
+
 sign = 1;
 init();
-addBoat();
+addShip();
+addPirateShip();
+addCannonBall();
 animate();
 
+// Ship
+function addShip() {
+    const geometry = new THREE.SphereGeometry(shipRadius, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: 0xfadf1f});
+    ship = new THREE.Mesh(geometry, material);
+
+    const x = 0;
+    const y = shipRadius * 0.8;
+    const z = 0;
+
+    ship.position.set(x, y, z);
+    scene.add(ship);
+};
+
+// Pirate Ship
+function addPirateShip() {
+    const geometry = new THREE.SphereGeometry(shipRadius, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: pirateShipColor});
+    pirateShip = new THREE.Mesh(geometry, material);
+
+    var location = random_spawn();
+    const x = location[0];
+    const y = pirateShipRadius * 0.8;
+    const z = location[2];
+
+    console.log(x);
+
+    pirateShip.position.set(x, y, z);
+    scene.add(pirateShip);
+};
+
+// Cannon Ball
+function addCannonBall() {
+    const geometry = new THREE.SphereGeometry(cannonBallRadius, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: cannonBallColor});
+    cannonBall = new THREE.Mesh(geometry, material);
+
+    var location = random_spawn();
+    const x = 0;
+    const y = shipRadius * 0.8;
+    const z = -10;
+
+    cannonBall.position.set(ship.position.x, ship.position.y + shipRadius, ship.position.z);
+    scene.add(cannonBall);
+};
+
+// Fire Cannon
+function fireCannonBall() {
+    cannonDirection = direction(ship, camera);
+    cannonBallFired = true;
+    // cannonBall.position.x += pos.x;
+    // cannonBall.position.z += pos.z;
+}
+
+// Move Cannonball
+function moveCannonBall() {
+    if(cannonBallFired)
+    {
+        cannonBall.position.x += cannonDirection.x * cannonBallSpeed;
+        cannonBall.position.z += cannonDirection.z * cannonBallSpeed;
+    }
+
+    let d = ship.position.distanceTo(cannonBall.position);
+
+    if(d > 2000)
+    {
+        cannonBallFired = false;
+        cannonBall.position.set(ship.position.x, ship.position.y + shipRadius, ship.position.z);
+    }
+
+    let dtps = pirateShip.position.distanceTo(cannonBall.position)
+    if(dtps < 20)
+    {
+        pirateShipAlive = false;
+        scene.remove(pirateShip);
+        console.log("Hit!")
+    }
+    // console.log(dtps);
+}
+
+function movePirateShip() {
+    // Get direction: pirateShip ---> Ship (normalized)
+    targetDir = direction(ship, pirateShip);
+
+    // Move in that direction
+    pirateShip.position.x += targetDir.x * pirateShipSpeed * 1.6;
+    // pirateShip.position.y += targetDir.y * shipSpeed * 2;
+    pirateShip.position.z += targetDir.z * pirateShipSpeed * 1.6;
+}
+
+document.addEventListener('keydown', function(event) {
+    const speed = 30;
+    let pos = direction(ship, camera);
+    let angle = pos.angleTo(east);
+    angle *= ((camera.position.x < ship.position.x) - 0.5) * 2;
+    
+    // Move
+    switch(event.keyCode)
+    {
+        // Front
+        case keyW: 
+            move(ship, angle, 0);
+            move(camera, angle, 0);
+            move(cannonBall, angle, 0);
+            break;
+        // Left
+        case keyA: 
+            move(ship, angle, PI/2);
+            move(camera, angle, PI/2);
+            move(cannonBall, angle, PI/2);
+            break;
+        // Back
+        case keyS: 
+            move(ship, angle, PI);
+            move(camera, angle, PI);
+            move(cannonBall, angle, PI);
+            break;
+        // Right
+        case keyD: 
+            move(ship, angle, 3*PI/2);
+            move(camera, angle, 3*PI/2);
+            move(cannonBall, angle, 3*PI/2);
+            break;
+        // Shoot
+        case keySpace:
+            if(cannonBallFired)
+                break;
+            console.log("Fire Cannon!");
+            fireCannonBall();
+    }
+    controls.target.set(ship.position.x, ship.position.y, ship.position.z);
+
+    // stats();
+});
+
+// Animation
+function animate() {
+    requestAnimationFrame(animate);
+
+    skyUniforms["mieDirectionalG"].value += sunVol * sign;
+    if(skyUniforms["mieDirectionalG"].value >= 2)
+    {
+        sign = -1;
+    }
+    if(skyUniforms["mieDirectionalG"].value <= 0.5)
+    {
+        sign = 1;
+    }
+
+    movePirateShip();
+    moveCannonBall();
+    // cannonBall.position.set(ship.position.x, ship.position.y + shipRadius, ship.position.z);
+    render();
+}
+
 function init() {
+    // Scene
+    scene = new THREE.Scene();
+
+    // Renderer
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
     document.body.appendChild(renderer.domElement);
 
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(
-        70,
-        window.innerWidth / window.innerHeight,
-        1,
-        20000
-    );
-    camera.position.set(0, hover, radius*2);
+    const fov = 70;
+    const aspectRatio = window.innerWidth / window.innerHeight;
+    const near = 1;
+    const far = 20000;
+
+    camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
+    camera.position.set(0, hover*3, shipRadius*7);
 
     sun = new THREE.Vector3();
 
@@ -135,52 +326,17 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-
-    skyUniforms["mieDirectionalG"].value += sunVol * sign;
-    if(skyUniforms["mieDirectionalG"].value >= 2)
-    {
-        sign = -1;
-    }
-    if(skyUniforms["mieDirectionalG"].value <= 0.5)
-    {
-        sign = 1;
-    }
-
-    render();
-}
-
 function render() {
     water.material.uniforms["time"].value += 1.0 / 60.0;
     renderer.render(scene, camera);
 }
 
-// Boat
-function addBoat() {
-    const mass = 50000;
-    const volume = 4/3*PI*(radius**3);
-    const density = mass/volume;
-    const buoyancy = 1;
-
-    const geometry = new THREE.SphereGeometry(radius, 24, 24);
-    const material = new THREE.MeshStandardMaterial({color: 0xfadf1f});
-    boat = new THREE.Mesh(geometry, material);
-
-    const x = 0;
-    const y = density*buoyancy;
-    const z = 0;
-
-    boat.position.set(x, y/2, z);
-    scene.add(boat);
-};
-
-function direction() {
+function direction(A, B) {
     let pos = new THREE.Vector3();
 
-    pos.x = boat.position.x - camera.position.x;
-    pos.y = boat.position.y - camera.position.y;
-    pos.z = boat.position.z - camera.position.z;
+    pos.x = A.position.x - B.position.x;
+    pos.y = 0
+    pos.z = A.position.z - B.position.z;
 
     pos.normalize();
     return pos;
@@ -188,45 +344,28 @@ function direction() {
 
 function move(obj, angle, phi)
 {
-    obj.position.x += boatSpeed * Math.sin(angle - phi);
-    obj.position.z -= boatSpeed * Math.cos(angle - phi);
+    obj.position.x += shipSpeed * Math.sin(angle - phi);
+    obj.position.z -= shipSpeed * Math.cos(angle - phi);
 }
-
-document.addEventListener('keydown', function(event) {
-    const speed = 30;
-    let pos = direction();
-    let angle = pos.angleTo(east);
-    angle *= ((camera.position.x < boat.position.x) - 0.5) * 2;
-    let conv = 3.14159/180
-    // let degree = angle/conv;
-    
-    // Move
-    switch(event.keyCode)
-    {
-        // Front
-        case keyW: move(boat, angle, 0);
-                   move(camera, angle, 0);
-                   break;
-        // Left
-        case keyA: move(boat, angle, PI/2);
-                   move(camera, angle, PI/2);
-                   break;
-        case keyS: move(boat, angle, PI);
-                   move(camera, angle, PI);
-                   break;
-        case keyD: move(boat, angle, 3*PI/2);
-                   move(camera, angle, 3*PI/2);
-                   break;
-    }
-    controls.target.set(boat.position.x, boat.position.y, boat.position.z);
-    stats();
-});
 
 function stats() {
     // Stats
     console.clear();
     console.log(skyUniforms["mieDirectionalG"].value);
-    // console.log("Boat Pos : ", boat.position);
+    // console.log("Ship Pos : ", ship.position);
     // console.log("Camera Pos : ", camera.position);
-    // console.log("Distance", boat.position.distanceTo(camera.position));
+    // console.log("Distance", ship.position.distanceTo(camera.position));
+}
+
+// Random spawn position
+function random_integer(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function random_spawn() {
+    let x = random_integer(-horizonDistance * distanceFactor, horizonDistance * distanceFactor);
+    let y = shipRadius * 0.8;
+    let z = random_integer(-horizonDistance * distanceFactor, horizonDistance * distanceFactor);
+
+    return [x, y, z];
 }
